d10d514e894aec49e441a3587714920d906233e1
ardat11 <197arda@gmail.com>
2023-10-20 19:15:31 +0300

    Day 4 of solving simple Python Problems

---
+++ Day_4/countdown.py
@@ -0,0 +1,9 @@
+# This code counts down to 0 from the input
+
+
+
+i = int(input())
+
+while i >=0:
+    print(i)
+    i -=1
---
+++ Day_4/exponentiation.py
@@ -0,0 +1,9 @@
+# This code takes the exponent of the numbers below your input until raching zero.
+
+
+
+if __name__ == '__main__':
+    n = int(input())
+
+for numbers in range(0, n):
+    print(numbers ** 2)
